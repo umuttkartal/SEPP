877d08625f944648f8d3e34eda55e9dfcb784446
Umut Kartal <ukartal05@gmail.com>
2021-04-24 19:06:21 +0100

    added Generators file

---
+++ src/test/java/shield/Generators.java
@@ -0,0 +1,16 @@
+package shield;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class Generators {
+    public static String generateCHI() {
+        Random rand = new Random();
+        int daysInPast = rand.nextInt(365 * 200);
+        int extension = rand.nextInt(8999) + 1000;
+        String date_of_birth =
+                LocalDate.now().minusDays(daysInPast).format(DateTimeFormatter.ofPattern("ddMMyy"));
+        return date_of_birth + extension;
+    }
+}
