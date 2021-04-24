bce85e810a37ea853dfa588b19c0a13258db492e
Umut Kartal <ukartal05@gmail.com>
2021-04-24 19:07:34 +0100

    small fix

--- src/test/java/shield/Generators.java
+++ src/test/java/shield/Generators.java
@@ -5,12 +5,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class Generators {
-    public static String generateCHI() {
-        Random rand = new Random();
-        int daysInPast = rand.nextInt(365 * 200);
-        int extension = rand.nextInt(8999) + 1000;
-        String date_of_birth =
-                LocalDate.now().minusDays(daysInPast).format(DateTimeFormatter.ofPattern("ddMMyy"));
-        return date_of_birth + extension;
-    }
+  public static String generateCHI() {
+    Random rand = new Random();
+    int daysInPast = rand.nextInt(365 * 200);
+    int extension = rand.nextInt(8999) + 1000;
+    String date_of_birth =
+            LocalDate.now().minusDays(daysInPast).format(DateTimeFormatter.ofPattern("ddMMyy"));
+    return date_of_birth + extension;
+  }
 }
